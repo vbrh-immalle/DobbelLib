5410c72e866aef6b95e1407c64a6ceb858e4d1c8
hansvb_immalle <hans.vanbroeckhoven@immalle.be>
2017-02-16 13:11:08 +0100

    DobbelLib + Tests

---
+++ DobbelLib/Dobbelsteen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobbelLib
+{
+    public class Dobbelsteen
+    {
+        private static Random rndGen = new Random();
+
+        private int waarde = 0;
+
+        public void Werp()
+        {
+            waarde = rndGen.Next(6) + 1;
+        }
+
+        public int Waarde
+        {
+            get
+            {
+                return waarde;
+            }
+        }
+    }
+}
---
+++ DobbelLibTest/DobbelsteenTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DobbelLib;
+
+namespace DobbelLibTest
+{
+    [TestClass]
+    public class DobbelsteenTest
+    {
+        [TestMethod]
+        public void TestDobbelsteenWaardeNaConstructie()
+        {
+            var d = new Dobbelsteen();
+
+            Assert.AreEqual(0, d.Waarde);
+        }
+
+        [TestMethod]
+        public void TestDobbelsteenWaardeNaWorp()
+        {
+            var d = new Dobbelsteen();
+
+            for(var i=0; i<20; i++)
+            {
+                d.Werp();
+                Assert.IsTrue(d.Waarde > 0 && d.Waarde < 7);
+            }
+        }
+    }
+}
